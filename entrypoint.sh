1f6d561361b0ba195c278ac99db81463ef45dcaa
Jesse L.K. Overton <jesse@ovrtn.com>
2019-03-12 03:13:47 -0400

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ echo "$SSH_KEY_PRIVATE" > "$SSH_KEY_PRIVATE_PATH"
 echo "$SSH_KEY_PUBLIC" > "$SSH_KEY_PUBLIC_PATH"
 
 chmod 700 "$SSH_PATH"
-chmod 600 "$WPENGINE_SSH_KEY_PRIVATE_PATH"
-chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
+chmod 600 "$SSH_KEY_PRIVATE_PATH"
+chmod 644 "$SSH_KEY_PUBLIC_PATH"
 
 docker-machine create --driver generic --generic-ip-address=$DOCKER_MACHINE_IP --generic-ssh-key=$SSH_KEY_PRIVATE_PATH --generic-ssh-user=$SSH_USER --engine-storage-driver=overlay $DOCKER_MACHINE_NAME
