254a83d8f7fe0367c4b9626fb59e9eacce8fa35b
Jesse L.K. Overton <jesse@ovrtn.com>
2019-03-12 03:05:54 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM docker:stable
+
+LABEL "com.github.actions.name"="GitHub Action Docker Machine"
+LABEL "com.github.actions.description"="An action for docker-machine"
+LABEL "com.github.actions.icon"="chevrons-right"
+LABEL "com.github.actions.color"="blue"
+
+LABEL "repository"="http://github.com/jovrtn/github-action-wpengine-git-deploy"
+LABEL "maintainer"="Jesse L.K. Overton <jesse@ovrtn.com>"
+
+ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+
+set -e
+
+SSH_PATH="$HOME/.ssh"
+
+mkdir "$SSH_PATH"
+
+SSH_KEY_PRIVATE_PATH="$SSH_PATH/id_rsa"
+SSH_KEY_PUBLIC_PATH="$SSH_PATH/id_rsa.pub"
+
+chmod 700 "$SSH_PATH"
+chmod 600 "$WPENGINE_SSH_KEY_PRIVATE_PATH"
+chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
+
+echo "$SSH_KEY_PRIVATE" > "$SSH_KEY_PRIVATE_PATH"
+echo "$SSH_KEY_PUBLIC" > "$SSH_KEY_PUBLIC_PATH"
+
+docker-machine create --driver generic --generic-ip-address=$DOCKER_MACHINE_IP --generic-ssh-key=$SSH_KEY_PRIVATE_PATH --generic-ssh-user=$SSH_USER --engine-storage-driver=overlay $DOCKER_MACHINE_NAME
