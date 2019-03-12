8e5a7776aa0f0d017d81b30f0b6c73e7dc10b790
Jesse L.K. Overton <jesse@ovrtn.com>
2019-03-12 03:16:49 -0400

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+base=https://github.com/docker/machine/releases/download/v0.16.0 && curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine && sudo install /tmp/docker-machine /usr/local/bin/docker-machine
+
 SSH_PATH="$HOME/.ssh"
 
 mkdir "$SSH_PATH"
